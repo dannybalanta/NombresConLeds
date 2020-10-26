801fc30b5971f038ce67972476595e6e7ad371c0
dannybalanta <42857316+dannybalanta@users.noreply.github.com>
2020-10-26 11:22:33 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,62 @@
+basic.forever(function () {
+    basic.showLeds(`
+        # # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # .
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # # # # #
+        # . . . #
+        # . . . #
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(1000)
+    basic.showString(",")
+    basic.showString("E")
+    basic.pause(2000)
+    basic.showString("D")
+    basic.pause(2000)
+    basic.showString("A")
+    basic.pause(2000)
+    basic.showString("L")
+    basic.pause(2000)
+    basic.showString("I")
+    basic.pause(2000)
+    basic.showString("P")
+    basic.pause(2000)
+    basic.showString("C")
+    basic.pause(2000)
+    basic.showString("E")
+    basic.pause(2000)
+    basic.showString("#")
+    basic.clearScreen()
+    basic.pause(5000)
+})
